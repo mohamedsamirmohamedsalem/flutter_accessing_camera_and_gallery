aa80e38840cc2d8ebd4c582e2cb8912dc3be2408
Mohamed Samir <mohamedsamirsalem100@gmail.com>
2020-01-22 10:36:33 +0200

    done

--- lib/OutlineButtonW.dart
+++ lib/OutlineButtonW.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'dart:io';
 
 /*Add the following keys to your Info.plist file,
 located in <project root>/ios/Runner/Info.plist:
@@ -12,65 +10,3 @@ Android #
 No configuration required - the plugin should work out of the box.
 
 Example #*/
-class OutlineButtonW extends StatefulWidget {
-  String buttontText;
-  ImageSource imageSource;
-  File imageFile;
-
-  OutlineButtonW(this.buttontText, this.imageSource);
-
-  @override
-  _OutlineButtonWState createState() => _OutlineButtonWState();
-
-  Widget showImageIfAvailable() {
-    return imageFile == null
-        ? Text('Please pick an image')
-        : Image.file(
-            imageFile,
-            fit: BoxFit.cover,
-            alignment: Alignment.topCenter,
-          );
-  }
-}
-
-class _OutlineButtonWState extends State<OutlineButtonW> {
-  void getImage(BuildContext context, ImageSource source) {
-    //maxwidth : for not making image to be high resolution for preventing app from crash in some cases
-    ImagePicker.pickImage(source: source, maxWidth: 400.0).then((File image) {
-      widget.imageFile = image;
-      Navigator.pop(context);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    Color defaultColor = Theme.of(context).accentColor;
-    return OutlineButton(
-      borderSide: BorderSide(
-        color: defaultColor,
-        width: 2.0,
-      ),
-      onPressed: () {
-        getImage(context, widget.imageSource);
-      },
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Icon(
-            Icons.camera_alt,
-            color: defaultColor,
-          ),
-          SizedBox(
-            width: 10.0,
-          ),
-          Text(
-            widget.buttontText,
-            style: TextStyle(
-              color: defaultColor,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_accessing_camera/OutlineButtonW.dart';
 import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 void main() => runApp(AccessCamera());
 
-class AccessCamera extends StatelessWidget {
+class AccessCamera extends StatefulWidget {
+  @override
+  _AccessCameraState createState() => _AccessCameraState();
+}
+
+class _AccessCameraState extends State<AccessCamera> {
+  File imageFile;
+  //
+  Widget showImageIfAvailable() {
+    return imageFile == null
+        ? Text('Please pick an image')
+        : Image.file(
+            imageFile,
+            height: 300.0,
+            fit: BoxFit.cover,
+            alignment: Alignment.topCenter,
+          );
+  }
+
+  void getImage(ImageSource source) {
+    //maxwidth : for not making image to be high resolution for preventing app from crash in some cases
+    ImagePicker.pickImage(source: source, maxWidth: 400.0).then((File image) {
+      setState(() {
+        imageFile = image;
+        //file path in memory
+        print('daaaaaaaaata $imageFile');
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    OutlineButtonW s = OutlineButtonW('Add Camera Image', ImageSource.camera);
+    Color defaultColor = Theme.of(context).accentColor;
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
@@ -18,12 +47,65 @@ class AccessCamera extends StatelessWidget {
               width: 300.0,
               child: Column(
                 children: <Widget>[
-                  s,
-                  OutlineButtonW('Pick Gallery Image', ImageSource.gallery),
-                  s.showImageIfAvailable(),
+                  OutlineButton(
+                    borderSide: BorderSide(
+                      color: defaultColor,
+                      width: 2.0,
+                    ),
+                    onPressed: () {
+                      getImage(ImageSource.camera);
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.camera_alt,
+                          color: defaultColor,
+                        ),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        Text(
+                          'Add Camera Image',
+                          style: TextStyle(
+                            color: defaultColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  OutlineButton(
+                    borderSide: BorderSide(
+                      color: defaultColor,
+                      width: 2.0,
+                    ),
+                    onPressed: () {
+                      getImage(ImageSource.gallery);
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.camera_alt,
+                          color: defaultColor,
+                        ),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        Text(
+                          'Choose from gallery',
+                          style: TextStyle(
+                            color: defaultColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                   SizedBox(
                     height: 30.0,
                   ),
+                  showImageIfAvailable(),
+
                   //   showImageIfAvailable,
                 ],
               ),
